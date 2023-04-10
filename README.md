734e495e2a1293e1a1c959adb7a23935baf2c41b
Gjorgji Dimeski <dimeskigj@gmail.com>
2023-04-10 23:36:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_drug_registry
+
+A Flutter project.
---
+++ android/app/src/main/kotlin/com/dimeskigj/drugregistry/flutter_drug_registry/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dimeskigj.drugregistry.flutter_drug_registry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
