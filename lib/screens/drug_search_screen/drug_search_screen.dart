2a1dcbe6b0edecb12a1d68ae7eb7ee8883933e09
Gjorgji Dimeski <dimeskigj@gmail.com>
2023-04-18 12:14:27 +0200

    [FTR] Add snapping search bar

--- lib/screens/drug_search_screen/drug_search_screen.dart
+++ lib/screens/drug_search_screen/drug_search_screen.dart
@@ -17,74 +17,84 @@ class DrugSearchScreen extends StatelessWidget {
       child: Builder(
         builder: (context) => SafeArea(
           child: Scaffold(
-            body: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                SearchBar(
-                  textEditingController: context.read<DrugSearchScreenViewModel>().textEditingController,
-                  hintText: 'Пребарувај лекови...',
+            body: CustomScrollView(
+              controller: context.read<DrugSearchScreenViewModel>().scrollController,
+              slivers: [
+                SliverAppBar(
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  floating: true,
+                  snap: true,
+                  pinned: false,
+                  flexibleSpace: SearchBar(
+                    textEditingController: context.read<DrugSearchScreenViewModel>().textEditingController,
+                    hintText: 'Пребарувај лекови...',
+                  ),
                 ),
                 // Hasn't searched yet
                 if (!context.watch<DrugSearchScreenViewModel>().hasSearched)
-                  const SvgStatus(
-                    assetName: 'assets/search_character.svg',
-                    headline: 'Започнете со пребарување',
-                    body: 'Пребарувајте лекови по име, генерик, или по состав.',
+                  const SliverToBoxAdapter(
+                    child: SvgStatus(
+                      assetName: 'assets/search_character.svg',
+                      headline: 'Започнете со пребарување',
+                      body: 'Пребарувајте лекови по име, генерик, или по состав.',
+                    ),
                   ),
                 // No results
                 if (context.watch<DrugSearchScreenViewModel>().hasNoResults)
-                  const SvgStatus(
-                    assetName: 'assets/no_results_character.svg',
-                    headline: 'Нема резултати од пребарувањето',
-                    body: 'Проверете дали точно сте го внеле терминот,или пак пробајте да го измените. '
-                        'Ако повторно не успеете, можеби го немаме бараниот лек запишано во базата.',
+                  const SliverToBoxAdapter(
+                    child: SvgStatus(
+                      assetName: 'assets/no_results_character.svg',
+                      headline: 'Нема резултати од пребарувањето',
+                    ),
                   ),
-                Flexible(
-                  fit: FlexFit.loose,
-                  child: ListView(
-                    controller: context.read<DrugSearchScreenViewModel>().scrollController,
-                    children: [
-                      ...context.watch<DrugSearchScreenViewModel>().searchResults.map((d) => DrugCard(drug: d)),
-                      // Loading indicator
-                      if (context.watch<DrugSearchScreenViewModel>().isLoading)
-                        Center(
-                          child: LoadingAnimationWidget.prograssiveDots(color: Theme.of(context).primaryColor, size: 50),
-                        ),
-                      // End of results
-                      if (context.watch<DrugSearchScreenViewModel>().isAtEndOfResults)
-                        Container(
-                          margin: const EdgeInsets.all(12),
-                          child: Text(
-                            'Крај на резултати.',
-                            style: Theme.of(context).textTheme.labelLarge,
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      // Retry
-                      if (context.watch<DrugSearchScreenViewModel>().hasError)
-                        GestureDetector(
-                          onTap: () => context.read<DrugSearchScreenViewModel>().retry(),
-                          child: Container(
-                            margin: const EdgeInsets.all(12),
-                            child: RichText(
-                              textAlign: TextAlign.center,
-                              text: TextSpan(
-                                style: Theme.of(context).textTheme.labelLarge,
-                                children: [
-                                  const TextSpan(text: 'Се случи грешка. '),
-                                  TextSpan(
-                                    text: 'Обидете се повторно',
-                                    style: Theme.of(context).textTheme.labelLarge?.copyWith(color: Theme.of(context).primaryColor),
-                                  ),
-                                  const TextSpan(text: '.'),
-                                ],
-                              ),
-                            ),
-                          ),
-                        )
-                    ],
-                  ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                      (context, index) => DrugCard(drug: context.read<DrugSearchScreenViewModel>().searchResults[index]),
+                      childCount: context.watch<DrugSearchScreenViewModel>().searchResults.length),
                 ),
+                // Loading indicator
+                if (context.watch<DrugSearchScreenViewModel>().isLoading)
+                  SliverToBoxAdapter(
+                    child: Center(
+                      child: LoadingAnimationWidget.prograssiveDots(color: Theme.of(context).primaryColor, size: 50),
+                    ),
+                  ),
+                // End of results
+                if (context.watch<DrugSearchScreenViewModel>().isAtEndOfResults)
+                  SliverToBoxAdapter(
+                    child: Container(
+                      margin: const EdgeInsets.all(12),
+                      child: Text(
+                        'Крај на резултати.',
+                        style: Theme.of(context).textTheme.labelLarge,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                // Retry
+                if (context.watch<DrugSearchScreenViewModel>().hasError)
+                  SliverToBoxAdapter(
+                    child: GestureDetector(
+                      onTap: () => context.read<DrugSearchScreenViewModel>().retry(),
+                      child: Container(
+                        margin: const EdgeInsets.all(12),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.labelLarge,
+                            children: [
+                              const TextSpan(text: 'Се случи грешка. '),
+                              TextSpan(
+                                text: 'Обидете се повторно',
+                                style: Theme.of(context).textTheme.labelLarge?.copyWith(color: Theme.of(context).primaryColor),
+                              ),
+                              const TextSpan(text: '.'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
               ],
             ),
           ),
