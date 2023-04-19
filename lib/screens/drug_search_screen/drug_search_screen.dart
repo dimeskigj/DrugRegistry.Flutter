14b6cd4cfd8ed08b7f58f69477d929f0f92b0fea
Gjorgji Dimeski <dimeskigj@gmail.com>
2023-04-19 14:01:50 +0200

    [DEV] Migrate search results scroll view to separate widget

--- lib/screens/drug_search_screen/drug_search_screen.dart
+++ lib/screens/drug_search_screen/drug_search_screen.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_drug_registry/screens/drug_search_screen/drug_search_screen_viewmodel.dart';
-import 'package:flutter_drug_registry/widgets/search_bar.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:flutter_drug_registry/widgets/drug_card.dart';
+import 'package:flutter_drug_registry/widgets/search_scroll_view.dart';
 import 'package:provider/provider.dart';
-
-import '../../widgets/diffutil_sliverlist_widget.dart';
-import '../../widgets/drug_card.dart';
-import '../../widgets/svg_status.dart';
 
 class DrugSearchScreen extends StatelessWidget {
   const DrugSearchScreen({super.key});
@@ -18,101 +14,18 @@ class DrugSearchScreen extends StatelessWidget {
       child: Builder(
         builder: (context) => SafeArea(
           child: Scaffold(
-            body: CustomScrollView(
-              controller: context.read<DrugSearchScreenViewModel>().scrollController,
-              slivers: [
-                SliverAppBar(
-                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                  floating: true,
-                  snap: true,
-                  pinned: false,
-                  flexibleSpace: SearchBar(
-                    textEditingController: context.read<DrugSearchScreenViewModel>().textEditingController,
-                    hintText: 'Пребарувај лекови...',
-                  ),
-                ),
-                DiffUtilSliverList.fromKeyedWidgetList(
-                  children: [
-                    ...context.watch<DrugSearchScreenViewModel>().searchResults.map((d) => DrugCard(drug: d, key: Key(d.id)))
-                  ],
-                  insertAnimationDuration: const Duration(milliseconds: 250),
-                  insertAnimationBuilder: (context, animation, child) => FadeTransition(
-                    opacity: animation,
-                    child: SlideTransition(
-                      position: Tween<Offset>(
-                        begin: const Offset(0, 0.2),
-                        end: Offset.zero,
-                      ).animate(animation),
-                      child: child,
-                    ),
-                  ),
-                  removeAnimationDuration: Duration.zero,
-                  removeAnimationBuilder: (context, animation, child) => child,
-                ),
-                // Hasn't searched yet
-                if (!context.watch<DrugSearchScreenViewModel>().hasSearched)
-                  const SliverToBoxAdapter(
-                    child: SvgStatus(
-                      assetName: 'assets/search_character.svg',
-                      headline: 'Започнете со пребарување',
-                      body: 'Пребарувајте лекови по име, генерик, или по состав.',
-                    ),
-                  ),
-                // No results
-                if (context.watch<DrugSearchScreenViewModel>().hasNoResults)
-                  const SliverToBoxAdapter(
-                    child: SvgStatus(
-                      assetName: 'assets/no_results_character.svg',
-                      headline: 'Нема резултати од пребарувањето',
-                    ),
-                  ),
-                // Loading indicator
-                if (context.watch<DrugSearchScreenViewModel>().isLoading)
-                  SliverToBoxAdapter(
-                    child: Container(
-                      margin: const EdgeInsets.only(bottom: 100),
-                      child: Center(
-                        child: LoadingAnimationWidget.prograssiveDots(color: Theme.of(context).primaryColor, size: 50),
-                      ),
-                    ),
-                  ),
-                // End of results
-                if (context.watch<DrugSearchScreenViewModel>().isAtEndOfResults)
-                  SliverToBoxAdapter(
-                    child: Container(
-                      margin: const EdgeInsets.all(12),
-                      child: Text(
-                        'Крај на резултати.',
-                        style: Theme.of(context).textTheme.labelLarge,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                // Retry
-                if (context.watch<DrugSearchScreenViewModel>().hasError)
-                  SliverToBoxAdapter(
-                    child: GestureDetector(
-                      onTap: () => context.read<DrugSearchScreenViewModel>().retry(),
-                      child: Container(
-                        margin: const EdgeInsets.all(12),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            style: Theme.of(context).textTheme.labelLarge,
-                            children: [
-                              const TextSpan(text: 'Се случи грешка. '),
-                              TextSpan(
-                                text: 'Обидете се повторно',
-                                style: Theme.of(context).textTheme.labelLarge?.copyWith(color: Theme.of(context).primaryColor),
-                              ),
-                              const TextSpan(text: '.'),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-              ],
+            body: SearchScrollView(
+              results: [...context.watch<DrugSearchScreenViewModel>().searchResults.map((e) => DrugCard(key: Key(e.id), drug: e))],
+              scrollController: context.read<DrugSearchScreenViewModel>().scrollController,
+              textEditingController: context.read<DrugSearchScreenViewModel>().textEditingController,
+              retryFunction: context.read<DrugSearchScreenViewModel>().retry,
+              hasSearched: context.watch<DrugSearchScreenViewModel>().hasSearched,
+              hasError: context.watch<DrugSearchScreenViewModel>().hasError,
+              hasNoResults: context.watch<DrugSearchScreenViewModel>().hasNoResults,
+              isLoading: context.watch<DrugSearchScreenViewModel>().isLoading,
+              isAtEndOfResults: context.watch<DrugSearchScreenViewModel>().isAtEndOfResults,
+              searchBarHintText: 'Пребарувај лекови...',
+              hasNotSearchedYetBodyText: 'Пребарувајте лекови по име, генерик или состав.',
             ),
           ),
         ),
