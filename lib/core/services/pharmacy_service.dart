5f6728a43da62971a3d3a12ef910d9e04180caef
Gjorgji Dimeski <dimeskigj@gmail.com>
2023-05-01 12:09:38 +0200

    [DEV] Add timeouts on pharmacy service

--- lib/core/services/pharmacy_service.dart
+++ lib/core/services/pharmacy_service.dart
@@ -8,13 +8,15 @@ import '../models/paged_result.dart';
 import '../models/pharmacy.dart';
 
 class PharmacyService {
+  final Duration _timeOut = const Duration(seconds: 10);
+
   Future<PagedResult<Pharmacy>> searchPharmacies(String query,
       {int page = 0, int size = 10, String? municipality, String? place}) async {
     final queryParameters = {'query': query, 'page': page.toString(), 'size': size.toString()};
     if (municipality?.isNotEmpty ?? false) queryParameters['municipality'] = municipality!;
     if (place?.isNotEmpty ?? false) queryParameters['place'] = place!;
     final url = Uri.https(Constants.baseApiUrl, 'api/pharmacies/search', queryParameters);
-    final response = await http.get(url);
+    final response = await http.get(url).timeout(_timeOut);
     response.ensureSuccessStatusCode();
     final json = jsonDecode(response.body);
     return PagedResult<Pharmacy>.fromJson(json);
@@ -31,7 +33,7 @@ class PharmacyService {
     if (municipality?.isNotEmpty ?? false) queryParameters['municipality'] = municipality!;
     if (place?.isNotEmpty ?? false) queryParameters['place'] = place!;
     final url = Uri.https(Constants.baseApiUrl, 'api/pharmacies/byLocation', queryParameters);
-    final response = await http.get(url);
+    final response = await http.get(url).timeout(_timeOut);
     response.ensureSuccessStatusCode();
     final json = jsonDecode(response.body);
     return PagedResult<Pharmacy>.fromJson(json);
@@ -39,7 +41,7 @@ class PharmacyService {
 
   Future<Iterable<String>> getMunicipalitiesByFrequency() async {
     final url = Uri.https(Constants.baseApiUrl, 'api/pharmacies/municipalitiesByFrequency');
-    final response = await http.get(url);
+    final response = await http.get(url).timeout(_timeOut);
     response.ensureSuccessStatusCode();
     final json = jsonDecode(response.body);
     return List<String>.from(json);
@@ -48,7 +50,7 @@ class PharmacyService {
   Future<Iterable<String>> getPlacesByFrequency(String municipality) async {
     final queryParameters = {'municipality': municipality};
     final url = Uri.https(Constants.baseApiUrl, 'api/pharmacies/placesByFrequency', queryParameters);
-    final response = await http.get(url);
+    final response = await http.get(url).timeout(_timeOut);
     response.ensureSuccessStatusCode();
     final json = jsonDecode(response.body);
     return List<String>.from(json);
