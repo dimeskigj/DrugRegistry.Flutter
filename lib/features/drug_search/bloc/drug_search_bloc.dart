109d95bc12940f86401393969dab7669a846c27a
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-07-02 19:25:17 +0200

    Implement blocs

--- lib/features/drug_search/bloc/drug_search_bloc.dart
+++ lib/features/drug_search/bloc/drug_search_bloc.dart
@@ -1,12 +1,44 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_drug_registry/core/models/drug.dart';
+import 'package:flutter_drug_registry/core/models/drug_group.dart';
+import 'package:flutter_drug_registry/core/services/drug_service.dart';
 
 part 'drug_search_event.dart';
 part 'drug_search_state.dart';
 
 class DrugSearchBloc extends Bloc<DrugSearchEvent, DrugSearchState> {
-  DrugSearchBloc() : super(DrugSearchInitial()) {
-    on<DrugSearchEvent>((event, emit) {
-    });
+  late final DrugService _drugService;
+
+  DrugSearchBloc(DrugService? drugService) : super(DrugSearchInitial()) {
+    _drugService = drugService ?? DrugService();
+
+    on<DrugSearchQueryChanged>(_onDrugSearchQueryChanged);
+    on<DrugSearchQuerySubmitted>(_onDrugSearchQuerySubmitted);
+  }
+
+  Future<void> _onDrugSearchQueryChanged(
+    DrugSearchQueryChanged event,
+    Emitter<DrugSearchState> emit,
+  ) async {
+    _queryDrugs(event.query, emit);
+  }
+
+  Future<void> _onDrugSearchQuerySubmitted(
+    DrugSearchQuerySubmitted event,
+    Emitter<DrugSearchState> emit,
+  ) async {
+    _queryDrugs(event.query, emit);
+  }
+
+  Future<void> _queryDrugs(String query, Emitter<DrugSearchState> emit) async {
+    try {
+      var results = await _drugService.searchDrugs(query, size: 50);
+      emit(DrugSearchLoadSuccess(drugs: results.data.toList()));
+    } catch (_) {
+      emit(const DrugSearchLoadFail());
+    }
   }
 }
--- lib/features/drug_search/bloc/drug_search_event.dart
+++ lib/features/drug_search/bloc/drug_search_event.dart
@@ -6,3 +6,39 @@ sealed class DrugSearchEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+sealed class DrugSearchQueryChanged extends DrugSearchEvent {
+  final String query;
+
+  const DrugSearchQueryChanged({required this.query});
+
+  @override
+  List<Object> get props => [query];
+}
+
+sealed class DrugSearchQuerySubmitted extends DrugSearchEvent {
+  final String query;
+
+  const DrugSearchQuerySubmitted({required this.query});
+
+  @override
+  List<Object> get props => [query];
+}
+
+sealed class DrugSearchSuggestionTapped extends DrugSearchEvent {
+  final DrugGroup drugGroup;
+
+  const DrugSearchSuggestionTapped({required this.drugGroup});
+
+  @override
+  List<Object> get props => [drugGroup];
+}
+
+sealed class DrugSearchDrugTapped extends DrugSearchEvent {
+  final Drug drug;
+
+  const DrugSearchDrugTapped({required this.drug});
+
+  @override
+  List<Object> get props => [drug.id];
+}
