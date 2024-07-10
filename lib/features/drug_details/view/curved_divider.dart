853853f59ff2ac757d8b0ca03e980fc3611a875f
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-07-11 00:11:53 +0200

    Remove buggy lines from divider

--- lib/features/drug_details/view/curved_divider.dart
+++ lib/features/drug_details/view/curved_divider.dart
@@ -7,36 +7,46 @@ class CurvedDivider extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Container(
-      color: theme.scaffoldBackgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            height: 10,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                bottomRight: Radius.circular(10),
-                bottomLeft: Radius.circular(10),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: theme.scaffoldBackgroundColor,
+          child: Transform.translate(
+            offset: Offset.fromDirection(-0.1),
+            child: Container(
+              height: 10,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  bottomRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                ),
+                color: theme.colorScheme.surface,
               ),
-              color: theme.colorScheme.surface,
             ),
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-            height: 10,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(10),
-                topLeft: Radius.circular(10),
+        ),
+        Container(
+          color: theme.scaffoldBackgroundColor,
+          height: 10,
+        ),
+        Container(
+          color: theme.scaffoldBackgroundColor,
+          child: Transform.translate(
+            offset: Offset.fromDirection(0.1),
+            child: Container(
+              height: 10,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  topLeft: Radius.circular(10),
+                ),
+                color: theme.colorScheme.surface,
               ),
-              color: theme.colorScheme.surface,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
